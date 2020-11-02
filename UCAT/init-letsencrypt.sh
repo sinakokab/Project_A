6d4b61c68d7253445d2b56f74d432713de240c2f
Sina K <15523694+sinakokab@users.noreply.github.com>
2020-11-02 13:14:34 +0000

    Update init-letsencrypt.sh

--- UCAT/init-letsencrypt.sh
+++ UCAT/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ucatexam.iwanttobeadentist.com www.ucatexam.iwanttobeadentist.com)
+domains=()
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
